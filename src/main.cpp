b4e1aae3728f7a1b253388739e2fdd055d1caf0c
James Gallagher <jgallagher@opendap.org>
2024-02-11 00:15:04 -0700

    Version 3 of the driver PCB
    Adds decouping caps and re-orders digits so LSD is on the right.

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,10 @@
 
 #define BAUD_RATE 9600 // 115200
 
+#ifndef VERSION_1_BOARD
+#define VERSION_1_BOARD 1
+#endif
+
 // Choose pins that cannot be used for hardware PWM
 int serialDataPin = 2;  // DS aka SER (serial data) input
 int clockPin = 4;       // SHCP aka SRCLK (shift register clock) input
@@ -22,9 +26,13 @@ int HV_Control = 5;  // PWM brightness 980Hz on pins 5 and 6, otherwise 480Hz
 ShiftRegister74HC595<1> sr(serialDataPin, clockPin, latchPin);
 
 // BCD for 0, ..., 9 for the LSD, MSD.
+#if VERSION_1_BOARD
+uint8_t MSD[10] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09};
+uint8_t LSD[10] = {0x00, 0x10, 0x20, 0x30, 0x40, 0x50, 0x60, 0x70, 0x80, 0x90};
+#else
 uint8_t LSD[10] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09};
 uint8_t MSD[10] = {0x00, 0x10, 0x20, 0x30, 0x40, 0x50, 0x60, 0x70, 0x80, 0x90};
-
+#endif
 
 void setup() {
     Serial.begin(BAUD_RATE);
